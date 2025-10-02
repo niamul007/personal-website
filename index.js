799ab1f3e66f0d6223677fe04226713d5f1adf3e
niamul007 <am3994566@gmail.com>
2025-10-02 13:25:49 +0600

    adding Splide slider function

---
+++ index.js
@@ -0,0 +1,8 @@
+var splide = new Splide('.splide', {
+  type       : 'loop',
+  perPage    : 1,
+  focus      : 'center',
+  gap        : '3rem',
+  fixedWidth : 600,
+  arrows     : true,
+}).mount();
